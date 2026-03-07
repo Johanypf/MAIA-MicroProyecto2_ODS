c690758be43a26369f7de838907c7a5666f5b1bd
Daniel Traslaviña <jdaniel.traslavina@gmail.com>
2026-03-07 01:09:09 -0500

    Comment out plotly chart configuration
    Comment out layout updates and chart display.

--- IMG_Classifier/streamlit_app.py
+++ IMG_Classifier/streamlit_app.py
@@ -97,6 +97,6 @@ if input_text is not None:
         st.error("Prediction failed")
         
     
-    fig.update_layout(showlegend=True)
-    fig.update_xaxes(tickmode="linear")
-    st.plotly_chart(fig)
+    #fig.update_layout(showlegend=True)
+    #fig.update_xaxes(tickmode="linear")
+    #st.plotly_chart(fig)
